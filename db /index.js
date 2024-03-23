2818c2fd2cd1f8ff5f4d44ce7766f557270b75cc
ivette-merc <ivettemerc@gmail.com>
2024-03-23 17:06:01 -0400

    db index and root index written

--- db /index.js
+++ db /index.js
@@ -1,11 +1,11 @@
-//init tables and info for database 
+//init tables and info for database
 
-const pg = require('pg');
+const pg = require("pg");
 const uuid = require("uuid");
 const client = new pg.Client(`postgres://localhost/${process.env.DB_NAME}`);
 
-const initTables = async () => {
-    const SQL = /*SQL*/ `
+const createTables = async () => {
+  const SQL = /*SQL*/ `
     DROP TABLE IF EXISTS reservation;
     DROP TABLE IF EXISTS customer;
     DROP TABLE IF EXISTS restaurant;
@@ -23,9 +23,120 @@ const initTables = async () => {
     CREATE TABLE reservation(
         id UUID PRIMARY KEY, 
         reservation_date TIMESTAMP NOT NULL DEFAULT now(),
-        customer_id UUID REFERENCES customer(id) NOT NULL.
+        customer_id UUID REFERENCES customer(id) NOT NULL,
+       customer_name VARCHAR(75) REFERENCES customer (name) NOT NULL,
         restaurant_id UUID REFERENCES restaurant(id) NOT NULL,
-    )
+       restaurant_name VARCHAR(75) REFERENCES restaurant (name) NOT NULL
+    );
     
     `;
-}
+  await client.query(SQL);
+};
+
+const createCustomer = async (name) => {
+  const SQL = /*SQL*/ `
+    INSERT INTO customer(id, name) VALUES ($1, $2) RETURNING *`;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  console.log("response-customer:", response);
+  return response.rows[0];
+};
+
+const createRestaurant = async (name) => {
+  const SQL = /*SQL*/ `
+    INSERT INTO restaurant(id, name) VALUES ($1, $2) RETURNING *`;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  console.log("response-restaurant:", response);
+  return response.rows[0];
+};
+
+const createReservation = async ({
+  customer_id,
+  customer_name,
+  restaurant_id,
+  restaurant_name,
+}) => {
+  const SQL = /*SQL*/ `
+   INSERT INTO reservation(id, customer_id,customer_name, restaurant_id, restaurant_name) VALUES($1, $2, $3, $4, $5) RETURNING *
+ 
+ `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    customer_id,
+    customer_name,
+    restaurant_id,
+    restaurant_name,
+  ]);
+  return response.rows[0];
+};
+
+const fetchReservation = async () => {
+  const SQL = /*SQL*/ `SELECT * from reservation`;
+  const { rows } = await client.query(SQL);
+  return rows;
+};
+
+const seed = async () => {
+  await Promise.all([
+    createCustomer("Ashley"),
+    createCustomer("Jo"),
+    createCustomer("Kelly"),
+    createRestaurant("Dunkin Donuts"),
+    createRestaurant("Tim Hortons"),
+    createRestaurant("Coffee Lodge"),
+  ]);
+  const customer = await fetchCustomer();
+  console.log("customers are:", await fetchCustomer());
+  const restaurant = await fetchRestaurant();
+  console.log("restaurants are:", await fetchRestaurant());
+
+  await Promise.all([
+    createReservation({
+      customer_id: customer[0].id,
+      customer_name: customer[0].name,
+      restaurant_id: restaurant[0].id,
+      restaurant_name: restaurant[0].name,
+      reservation_date: "3/23/24",
+    }),
+    createReservation({
+      customer_id: customer[1].id,
+      customer_name: customer[1].name,
+      restaurant_id: restaurant[1].id,
+      restaurant_name: restaurant[1].name,
+      reservation_date: "3/24/24",
+    }),
+    createReservation({
+      customer_id: customer[2].id,
+      customer_name: customer[2].name,
+      restaurant_id: restaurant[2].id,
+      restaurant_name: restaurant[2].name,
+      reservation_date: "3/25/24",
+    }),
+  ]);
+  console.log("reservations created:", await fetchReservation());
+};
+
+const fetchCustomer = async () => {
+  const SQL = /*SQL*/ `
+    SELECT * from customer;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const fetchRestaurant = async () => {
+  const SQL = /*SQL*/ `
+    SELECT * from restaurant;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+module.exports = {
+  client,
+  createTables,
+  createCustomer,
+  createRestaurant,
+  fetchCustomer,
+  fetchRestaurant,
+  seed,
+};
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 require('dotenv').config();
+const {client, createTables, seed} = require("./db ");
 const express = require('express');
-con
+
+const app = express();
+
+//middleware
+
+
+//init function
+const init = async () => {
+    await client.connect();
+    console.log('db connected');
+
+    await createTables();
+    console.log('tables created');
+
+    await seed();
+    console.log('data seeded');
+
+    
+
+};
+
+init();
